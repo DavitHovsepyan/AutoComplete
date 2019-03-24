7e9d4004663ccea6f31eba9c75cba87487f1b945
DavitHovsepyan <davit@simplytechnologies.net>
2019-03-25 01:58:33 +0400

    baseURL fixed

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,5 @@
-export const baseURL = 'https://autocomplete-backend.herokuapp.com/api';
+const development = process.env.NODE_ENV === 'development';
+
+export const baseURL = development 
+        ? 'http://localhost:5000/api'
+        : 'https://autocomplete-backend.herokuapp.com/api';
